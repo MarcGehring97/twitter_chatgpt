d87af6024343d80673b2236eee6a98ec11de5056
MarcGehring97 <marc_gehring@gmx.de>
2023-01-10 14:08:58 +0100

    commit

--- chatgpt.py
+++ chatgpt.py
@@ -1,5 +1,17 @@
-import os
 import openai
-openai.organization = "org-tYSqGUt1V24cqqFl6qYlG2Oq"
-openai.api_key = "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"
-openai.Model.list()
+
+openai.api_key = "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"
+
+def chatgpt(prompt):
+    # Generate a response
+    completion = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    response = completion.choices[0].text
+    return response
